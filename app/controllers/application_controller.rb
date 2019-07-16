d146a0ee2042e28e79ab8185b63ee7801d347840
Ariel <arielord@buffalo.edu>
2019-07-16 19:43:55 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,16 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
-
+  get '/recipes' do
+    # @recipe1 = Recipe.create(:name => "Bomb.com Mac and Cheese", :ingredients => "milk, butter, cheese, elbow pasta", :cook_time => "20 minutes")
+    # @recipe2 = Recipe.create(:name => "waldorf salad", :ingredients => "apples, cabbage, oil, vinegar", :cook_time => "0")
+    @recipes = Recipe.all
+    erb :index  
+  end
+  
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    erb :index
+  end
+  
 end
